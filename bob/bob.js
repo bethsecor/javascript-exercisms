adc63f3ca84b528da90639ae9f11acca9342c5f5
Beth Secor <elizabeth.a.secor@gmail.com>
2016-06-10 13:30:15 -0600

    Finish bob JS exercism

--- bob/bob.js
+++ bob/bob.js
@@ -1,14 +1,14 @@
 var Bob = function() {};
 
 Bob.prototype.hey = function(input) {
-  if (input === '') {
-    return 'Fine. Be that way!'
-  } else if (input.toUpperCase() === input) {
-    return 'Whoa, chill out!'
+  if (input.trim() === '') {
+    return 'Fine. Be that way!';
+  } else if (input.toUpperCase() === input && input.search(/[a-zA-Z]/) !== -1) {
+    return 'Whoa, chill out!';
   } else if (input.slice(-1) === "?") {
-    return 'Sure.'
+    return 'Sure.';
   } else {
-    return 'Whatever.'
+    return 'Whatever.';
   }
 };
 
